f75afbcca1414097e920c744e7a7ae9d205f6175
TheRealMarion1999 <sjniesen@madisoncollege.edu>
2026-02-09 10:14:31 -0600

    more work on project 1

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -3,6 +3,7 @@
 
 //temporary thing so that I'm able to actually autofill variables for a node.
 let table_set = false;
+const TABLE_VALUES = ["Gross Pay", "Federal Taxes", "State Taxes", "Medicare Taxes", "SSN Taxes", "Total Taxes", "Net Pay"];
 let table = document.getElementById("taxes-table");
 const init = () => {
     table = document.getElementById("taxes-table");
@@ -15,16 +16,26 @@ const init = () => {
     ButtonEventListener2.addEventListener("click", tax_calcs);
 }
 
-const tax_calcs = () => {
-    console.log("beef");
+const tax_calcs = event => {
+    let table = document.getElementById("taxes-table");
     //check if TRs exist
     //if they do, get rid of the current TRs and replace them with a new batch
     //populate each TR with 2 TDs
-    //names will likely have to be hardcoded (could use an array? Maybe? sounds jank.)
-    //
-}
+    for (i in TABLE_VALUES) {
+        //apparenlty this has to be here... kinda don't like that lol. In Godot I could just declare it globally and then call a .new() function on nodes.
+        let table_rows = document.createElement("tr");
+        console.log(TABLE_VALUES[i]);
+        //populate the taxes table
+        table.appendChild(table_rows);
 
-const tax_calcs_2024 = () => {
-    console.log("chicken")
+        
+    }
+
+    //this is a slightly more secure way of doing it, in the event we ever end up adding new buttons to the site
+    if (event.currentTarget.getAttribute("data-model") == "taxes2020") {
+        console.log("beef");
+    } else if (event.currentTarget.getAttribute("data-model") == "taxes2024") {
+        console.log("chicken");
+    }
 }
 document.addEventListener("DOMContentLoaded", init);
