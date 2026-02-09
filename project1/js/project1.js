fc63f08b48047e778c7f12bd49585fcd8e386bbf
TheRealMarion1999 <sjniesen@madisoncollege.edu>
2026-02-09 11:34:02 -0600

    dynamic table removal

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -4,6 +4,13 @@
 //temporary thing so that I'm able to actually autofill variables for a node.
 const TABLE_VALUES = ["Gross Pay", "Federal Taxes", "State Taxes", "Medicare Taxes", "SSN Taxes", "Total Taxes", "Net Pay"];
 let table = document.getElementById("taxes-table");
+let grossPay = 0;
+let fedTax = 0;
+let stateTax = 0;
+let medTax = 0;
+let SSNTax = 0;
+let totalTax = 0;
+let netPay = 0;
 const init = () => {
     table = document.getElementById("taxes-table");
 
@@ -21,8 +28,14 @@ const tax_calcs = event => {
     let userInput = document.getElementById("input");
     let userInputValue = userInput.value;
     if (userInputValue !== "") {
-        //check if TRs exist
+
+        console.log(get_table_size(table));
+        if (get_table_size(table) > 1) {
+            destroy_table_rows();
+        }
+        //check if TRs exist (or just... anything in table, really.)
         //if they do, get rid of the current TRs and replace them with a new batch
+        //destroy_table_rows();
         //populate each TR with 2 TDs
         //this is a slightly more secure way of doing it, in the event we ever end up adding new buttons to the site
         if (event.currentTarget.getAttribute("data-model") == "taxes2020") {
@@ -38,31 +51,43 @@ const tax_calcs = event => {
 
 //creates a table row with a value from the TABLE_VALUES array
 const create_table_row = (table, leftCellContent = "term", rightCellContent = "value") => {
-    let table_rows = document.createElement("tr");
+    let table_row = document.createElement("tr");
+    table_row.className = "infoRow";
     //(TODO: prolly could do this better)
     let table_data_L = document.createElement("td");
     let table_data_R = document.createElement("td");
     table_data_L.textContent = leftCellContent;
     table_data_R.textContent = rightCellContent;
-    table_rows.appendChild(table_data_L);
-    table_rows.appendChild(table_data_R);
-    table.appendChild(table_rows);
+    table_row.appendChild(table_data_L);
+    table_row.appendChild(table_data_R);
+    table.appendChild(table_row);
 }
 
-const tax_calcs_2020 = (money) => {
+//destroys all rows in the current array
+const destroy_table_rows = () => {
+    let table = document.getElementById("taxes-table");
 
-    let grossPay = 0;
-    let fedTax = 0;
-    let stateTax = 0;
-    let medTax = 0;
-    let SSNTax = 0;
-    let totalTax = 0;
-    let netPay = 0;
+    let child = table.lastElementChild;
+    while (child) {
+        table.removeChild(child);
+        child = table.lastElementChild;
+    }
+}
+
+//TODO: make the variables used here global
+const tax_calcs_2020 = (money) => {
     return [grossPay, fedTax, stateTax, medTax, SSNTax, totalTax, netPay];
 }
 
 const tax_calcs_2024 = (money) => {
-    console.log(money);
+    return [grossPay, fedTax, stateTax, medTax, SSNTax, totalTax, netPay];
+}
+
+const get_table_size = (table) => {
+    let table2 = document.getElementById("taxes-table");
+    let table_size = table.childNodes;
+
+    return table_size.length;
 }
 
 document.addEventListener("DOMContentLoaded", init);
