55c5e806f0a7fb288ba2f7d3c5b86f8de2054c27
Marion <sjniesen@madisoncollege.edu>
2026-02-16 15:04:51 -0600

    I fucking hate the American Tax System.

--- project1/js/project1.js
+++ project1/js/project1.js
@@ -1,37 +1,49 @@
-//due to the way that this is set up, it would be easy to take the content from this and create two functions that are called
-//on click. This should be pretty easy. Should be lol.
+//I'm looking at the rubric. I don't see "Navigate the American Tax system" on the listed skills I'm meant to be assessed on.
 
-//temporary thing so that I'm able to actually autofill variables for a node.
 const TABLE_VALUES = ["Gross Pay", "Federal Taxes", "State Taxes", "Medicare Taxes", "SSN Taxes", "Total Taxes", "Net Pay"];
 
+//FEDERAL TAX
 const FED_TAX_BRACKETS_2020 = [9876, 40126, 85526, 163301, 207351, 518401];
 const FED_TAX_BRACKETS_2024 = [11601, 47151, 100526, 191951, 243726, 609351];
 const FED_TAX_PERCENTAGES = [.1, .12, .22, .24, .32, .35, .37];
-const FED_TAX_REMAINDERS_2020 = [987.5, 5802.5, 24618, 63810, 130090, 245178];
-const FED_TAX_PREFILLED_2020 = [987.5, 5802.5, 24618, 63810, 130090, 245178];
-const FED_TAX_REMAINDERS_2024 = [11160, 47150, 100525, 39110.5, 55678.5, 183647.25];
-const FED_TAX_PREFILLED_2024 = [1160, 5426, 17168.5, 39110.5, 55678.5, 183647.25];
+const FED_TAX_REMAINDERS_2020 = [9875, 40125, 85525, 163300, 207350, 518400];
+const FED_TAX_PREFILLED_2020 = [FED_TAX_REMAINDERS_2020[0] * FED_TAX_PERCENTAGES[0],
+                                (FED_TAX_REMAINDERS_2020[1] - FED_TAX_REMAINDERS_2020[0]) * FED_TAX_PERCENTAGES[1],
+                                (FED_TAX_REMAINDERS_2020[2] - FED_TAX_REMAINDERS_2020[1]) * FED_TAX_PERCENTAGES[2],
+                                (FED_TAX_REMAINDERS_2020[3] - FED_TAX_REMAINDERS_2020[2]) * FED_TAX_PERCENTAGES[3],
+                                (FED_TAX_REMAINDERS_2020[4] - FED_TAX_REMAINDERS_2020[3]) * FED_TAX_PERCENTAGES[4],
+                                (FED_TAX_REMAINDERS_2020[5] - FED_TAX_REMAINDERS_2020[4]) * FED_TAX_PERCENTAGES[5]
+                                ];
+const FED_TAX_REMAINDERS_2024 = [11600, 47150, 100525, 191950, 243725, 609350];
+const FED_TAX_PREFILLED_2024 = [FED_TAX_REMAINDERS_2024[0] * FED_TAX_PERCENTAGES[0],
+                                (FED_TAX_REMAINDERS_2024[1] - FED_TAX_REMAINDERS_2024[0]) * FED_TAX_PERCENTAGES[1],
+                                (FED_TAX_REMAINDERS_2024[2] - FED_TAX_REMAINDERS_2024[1]) * FED_TAX_PERCENTAGES[2],
+                                (FED_TAX_REMAINDERS_2024[3] - FED_TAX_REMAINDERS_2024[2]) * FED_TAX_PERCENTAGES[3],
+                                (FED_TAX_REMAINDERS_2024[4] - FED_TAX_REMAINDERS_2024[3]) * FED_TAX_PERCENTAGES[4],
+                                (FED_TAX_REMAINDERS_2024[5] - FED_TAX_REMAINDERS_2024[4]) * FED_TAX_PERCENTAGES[5]
+                                ];
 
+//STATE TAX
 const WI_TAX_BRACKETS_2020 = [11971, 23931, 263481];
 const WI_TAX_BRACKETS_2024 = [12761, 25521, 280951];
 const WI_TAX_PERCENTAGES_2020 = [.0354, .0465, .0627, .0765];
 const WI_TAX_PERCENTAGES_2024 = [WI_TAX_PERCENTAGES_2020[0], WI_TAX_PERCENTAGES_2020[1], .053, WI_TAX_PERCENTAGES_2020[2]];
-const WI_TAX_REMAINDERS_2020 = [423.74, 979.88, 15999.67];
+const WI_TAX_REMAINDERS_2020 = [11970, 23930, 263480];
 const WI_TAX_PREFILLED_2020 = [423.74, 979.88, 15999.67];
-const WI_TAX_REMAINDERS_2024 = [451.7, 1045.04, 14582.83];
+const WI_TAX_REMAINDERS_2024 = [12760, 25520, 280951];
 const WI_TAX_PREFILLED_2024 = [451.7, 1045.04, 14582.83];
 
-//v- future proofing lol
-const SOCIAL_SECURITY_TAX_BRACKET = [.062];
+//SOCIAL SECURITY
+const SOCIAL_SECURITY_TAX_BRACKET = .062;
 const SOCIAL_SECURITY_TAX_LIMIT_2020 = 137000;
 const SOCIAL_SECURITY_MAX_PAY_2020 = 84940;
 const SOCIAL_SECURITY_TAX_LIMIT_2024 = 168000;
 const SOCIAL_SECURITY_MAX_PAY_2024 = 10453.2;
 
-const MEDICARE_TAX_LIMIT_2020 = 200000;
+//MEDICARE
+const MEDICARE_TAX_LIMIT = 200000;
 const MEDICARE_REMAINDER = 29000;
 const MEDICARE_TAX_BRACKETS = [.0145, .0235];
-const MEDICARE_TAX_LIMIT_2024 = 200000;
 const init = () => {
     //seemingly wasn't needed either; commented out just in case something actually did need it.
     //let table = document.getElementById("taxes-table");
@@ -54,12 +66,13 @@ const tax_calcs = event => {
         if (get_table_size(table) > 1) {
             destroy_table_rows(table);
         }
-        //populate each TR with 2 TDs
-        //this is a slightly more secure way of doing it, in the event we ever end up adding new buttons to the site
-        if (event.currentTarget.getAttribute("data-model") == "taxes2020") {
-            values = tax_calcs_2020(userInputValue);
-        } else if (event.currentTarget.getAttribute("data-model") == "taxes2024") {
-            values = tax_calcs_2024(userInputValue);
+        switch(event.currentTarget.getAttribute("data-model")) {
+            case "taxes2020":
+                values = tax_calcs_2020(userInputValue);
+                break;
+            case "taxes2024":
+                values = tax_calcs_2024(userInputValue);
+                break;
         }
         for (i in TABLE_VALUES) {
             create_table_row(table, TABLE_VALUES[i], values[i]);
@@ -91,27 +104,25 @@ const destroy_table_rows = (table) => {
     }
 }
 
-//TODO: replace magic numbers
-
 const tax_calcs_2020 = (money) => {
     money = Number(money);
-
+    console.log((FED_TAX_REMAINDERS_2020[2] * FED_TAX_PERCENTAGES[2]) + FED_TAX_PREFILLED_2020[1]);
     return [convert_to_string(money),
         convert_to_string(federal_tax_2020(money)), 
         convert_to_string(state_tax_2020(money)),
-        convert_to_string(medicare_tax_2020(money)),
+        convert_to_string(medicare_tax(money)),
         convert_to_string(social_security_tax_2020(money)),
         convert_to_string(calculate_total_tax(
                             federal_tax_2020(money), 
                             state_tax_2020(money),
-                            medicare_tax_2020(money),
+                            medicare_tax(money),
                             social_security_tax_2020(money)
                         )),
         convert_to_string(
                 money - calculate_total_tax(
-                social_security_tax_2020(money), 
+                federal_tax_2020(money), 
                 state_tax_2020(money), 
-                medicare_tax_2020(money), 
+                medicare_tax(money), 
                 social_security_tax_2020(money))
             )];
 }
@@ -138,7 +149,7 @@ const federal_tax_2020 = (money) => {
         remainder = money - FED_TAX_REMAINDERS_2020[0];
         tax = FED_TAX_PREFILLED_2020[0] + (remainder * FED_TAX_PERCENTAGES[1]);
     } else {
-        tax = money * FED_TAX_PERCENTAGES_2020[0];
+        tax = money * FED_TAX_PERCENTAGES[0];
     }
     return tax;
 }
@@ -158,16 +169,13 @@ const state_tax_2020 = (money) => {
     } else {
         tax = money * WI_TAX_PERCENTAGES_2020[0];
     }
-    if (isNaN(tax)) {
-        console.log(money);
-    }
     return tax;
 }
 
-const medicare_tax_2020 = (money) => {
+const medicare_tax = (money) => {
     let tax;
     let remainder;
-    if (money > MEDICARE_TAX_LIMIT_2020) {
+    if (money > MEDICARE_TAX_LIMIT) {
         remainder = money - MEDICARE_REMAINDER
         tax = MEDICARE_REMAINDER + (remainder * MEDICARE_TAX_BRACKETS[1]);
     } else {
@@ -181,7 +189,7 @@ const social_security_tax_2020 = (money) => {
     if (money > SOCIAL_SECURITY_TAX_LIMIT_2020) {
         tax = SOCIAL_SECURITY_MAX_PAY_2020;
     } else {
-        tax = money * SOCIAL_SECURITY_TAX_BRACKET[0];
+        tax = money * SOCIAL_SECURITY_TAX_BRACKET;
     }
     return tax;
 }
@@ -192,24 +200,23 @@ const tax_calcs_2024 = (money) => {
     return [convert_to_string(money),
         convert_to_string(federal_tax_2024(money)), 
         convert_to_string(state_tax_2024(money)),
-        convert_to_string(medicare_tax_2024(money)),
+        convert_to_string(medicare_tax(money)),
         convert_to_string(social_security_tax_2024(money)),
         //honestly, these are a little hard to read, but I decided to try and get zero variables in the main function just to test myself
         convert_to_string(calculate_total_tax(
                             federal_tax_2024(money), 
                             state_tax_2024(money),
-                            medicare_tax_2024(money),
+                            medicare_tax(money),
                             social_security_tax_2024(money)
                         )),
         convert_to_string(
                 money - calculate_total_tax(
                 social_security_tax_2024(money), 
                 state_tax_2024(money), 
-                medicare_tax_2024(money), 
+                medicare_tax(money), 
                 social_security_tax_2024(money))
             )];
 }
-//TODO: could unify 2020 and 2024 function with a switch?
 
 const federal_tax_2024 = (money) => {
     let tax
@@ -256,24 +263,13 @@ const state_tax_2024 = (money) => {
     return tax;
 }
 
-const medicare_tax_2024 = (money) => {
-    let tax
-    let remainder;
-    if (money > MEDICARE_TAX_LIMIT_2024) {
-        remainder = money - MEDICARE_REMAINDER;
-        tax = MEDICARE_REMAINDER + (remainder * MEDICARE_TAX_BRACKETS[1]);
-    } else {
-        tax = money * MEDICARE_TAX_BRACKETS[0];
-    }
-    return tax;
-}
 
 const social_security_tax_2024 = (money) => {
     let tax
     if (money > SOCIAL_SECURITY_TAX_LIMIT_2024) {
         tax = SOCIAL_SECURITY_MAX_PAY_2024;
     } else {
-        tax = money * SOCIAL_SECURITY_TAX_BRACKET[0];
+        tax = money * SOCIAL_SECURITY_TAX_BRACKET;
     }
     return tax;
 }
