889cfffdb22ba85e67be3b4e00f64068fd4df5af
Marion <sjniesen@madisoncollege.edu>
2026-02-22 14:03:38 -0600

    lab 5 finished

--- lab 5/js/json.js
+++ lab 5/js/json.js
@@ -1,5 +1,27 @@
 
-
 const init = () => {
-    
+    let students = [
+        {"name":"Johnson Brohnson", "id":"123456", "email":"jbrohnson@coolUniversity.edu"},
+        {"name":"Anna Golbi", "id":"242182", "email":"agolbi@coolUniversity.edu"},
+        {"name":"Andrew Andrews", "id":"124753" , "email":"aandrews@coolUniversity.edu"},
+        {"name":"Delroy Crisp", "id":"123156" , "email":"dcrisp@coolUniversity.edu"},
+        {"name":"Winston Begum", "id":"182563" , "email":"wbegum@coolUniversity.edu"}
+    ]
+    const CONTAINER = document.getElementById("container");
+    for(let i = 0; i < students.length; i++) {
+        console.log("student ID: " + students[i].id + "\nStudent email: " + students[i].email);
+        create_student_listing(CONTAINER, "student ID: " + students[i].id);
+        create_student_listing(CONTAINER, "Student email: " + students[i].email)
+    }
 }
+
+const create_student_listing = (container, text) => {
+    const PARAGRAPH = document.createElement("p");
+    PARAGRAPH.textContent = text;
+    container.appendChild(PARAGRAPH);
+
+}
+
+window.onload = init
+
+//document.addEventListener("DOMContentLoaded", init);
