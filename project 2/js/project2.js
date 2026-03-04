33cfb36cb5c9cae896caaf2434a70bf66949e336
TheRealMarion1999 <sjniesen@madisoncollege.edu>
2026-03-04 11:38:43 -0600

    oh that's what I had to do? LMAO

--- project 2/js/project2.js
+++ project 2/js/project2.js
@@ -1,5 +1,6 @@
 const USERNAME = "sjniesen";
-const COUNTRY = "USA";
+//TODO: For now, this is fine as just US, but getting different countries could be a worthwhile feature.
+const COUNTRY = "US";
 const ZIPCODE= 53562;
 
 const init = () => {
@@ -8,35 +9,60 @@ const init = () => {
 }
 
 const getLocation = () => {
+    let lat;
+    let lng;
     const URL = "http://api.geonames.org/postalCodeSearchJSON";
 
     const ENTERED_POSTAL_CODE = document.getElementById("zipCode");
     const POSTALCODE = ENTERED_POSTAL_CODE.value;
-    console.log(ZIPCODE);
+    console.log(POSTALCODE);
 
-    const PARAMS = `?username=${USERNAME}&postalcode=${ZIPCODE}&country=${COUNTRY}`;
+    const PARAMS = `?username=${USERNAME}&postalcode=${POSTALCODE}&country=${COUNTRY}`;
 
     const XHR = new XMLHttpRequest();
 
     console.log(URL + PARAMS);
 
-    XHR.open("get", URL + PARAMS);
+    XHR.open("get", URL + PARAMS, true);
 
     XHR.onload = () => {
         if (XHR.readyState === 4) {
             const DATA = JSON.parse(XHR.responseText);
-
-            console.log(DATA);
-            const LAT = DATA;
+            const LAT = DATA.postalCodes[0].lat;
+            const LNG = DATA.postalCodes[0].lng;
+            const PLACE = DATA.postalCodes[0].placeName;
+            const STATE = DATA.postalCodes[0].adminName1;
+            const COUNTY = DATA.postalCodes[0].adminName2;
+            //this is probably not a good way to do it. nesting XHRs just feels wrong, y'know?
+            getWeather(LAT, LNG);
         }
     }
-
     XHR.onerror = () => {
         console.log("network error");
     }
+    XHR.send(null);
 }
 
 const getWeather = (lat, lng) => {
+    const URL = "http://api.geonames.org/findNearByWeatherJSON";
+    const PARAMS = `?lat=${lat}&lng=${lng}&username=${USERNAME}`;
+
+    console.log(URL + PARAMS);
+    const XHR = new XMLHttpRequest();
+
+    XHR.open("get", URL + PARAMS, true);
+
+    XHR.onload = () => {
+        if (XHR.readyState === 4) {
+            const DATA = JSON.parse(XHR.responseText);
+            //this is great but... how am I gonna get this outta here?
+            console.log(DATA);
+        }
+    }
+    XHR.onerror = () => {
+        console.log("network error");
+    }
+    XHR.send(null);
 
 }
 
