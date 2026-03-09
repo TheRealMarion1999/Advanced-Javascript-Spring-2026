a82cee3cb640ad79327664186eaae3dacf40d5d0
Marion <sjniesen@madisoncollege.edu>
2026-03-09 14:01:31 -0500

    more project 2 work

--- project 2/js/project2.js
+++ project 2/js/project2.js
@@ -9,9 +9,10 @@ const init = () => {
 }
 
 const getLocation = () => {
-    let lat;
-    let lng;
     const URL = "http://api.geonames.org/postalCodeSearchJSON";
+
+    const MESSAGE = document.getElementById("loadingMessage");
+    MESSAGE.textContent = "Girls are conducting meteorology, please wait warmly...";
 
     const ENTERED_POSTAL_CODE = document.getElementById("zipCode");
     const POSTALCODE = ENTERED_POSTAL_CODE.value;
@@ -26,6 +27,7 @@ const getLocation = () => {
     XHR.open("get", URL + PARAMS, true);
 
     XHR.onload = () => {
+        
         if (XHR.readyState === 4) {
             const DATA = JSON.parse(XHR.responseText);
             const LAT = DATA.postalCodes[0].lat;
@@ -38,6 +40,7 @@ const getLocation = () => {
         }
     }
     XHR.onerror = () => {
+        MESSAGE.textContent = "network error while processing location";
         console.log("network error while processing location");
     }
     XHR.send(null);
@@ -46,6 +49,8 @@ const getLocation = () => {
 const getWeather = (lat, lng) => {
     const URL = "http://api.geonames.org/findNearByWeatherJSON";
     const PARAMS = `?lat=${lat}&lng=${lng}&username=${USERNAME}`;
+
+    const MESSAGE = document.getElementById("loadingMessage");
 
     console.log(URL + PARAMS);
     const XHR = new XMLHttpRequest();
@@ -57,12 +62,35 @@ const getWeather = (lat, lng) => {
             const DATA = JSON.parse(XHR.responseText);
             //this is great but... how am I gonna get this outta here?
             console.log(DATA);
+            const WIND_DIRECTON = DATA.weatherObservation.windDirection;
+            const WIND_SPEED = DATA.weatherObservation.windSpeed;
+            const TEMPERATURE = convert_to_burger_measurements(DATA.weatherObservation.temperature).toFixed(2);
+            console.log(TEMPERATURE);
+            //ohhhh ewwww am I really gonna have to put the node creation in here too...? gross...
+
         }
     }
     XHR.onerror = () => {
+        MESSAGE.textContent = "network error while processing weather data"
         console.log("network error while processing weather data");
     }
     XHR.send(null);
+
+    MESSAGE.textContent = ""
+    
+
+}
+
+const convert_to_burger_measurements = (celsius) => {
+    const FARENHEIT = celsius * (9.0/5.0) + 32;
+    return FARENHEIT;
+}
+
+const create_weather_information = (farenheit, windSpeed) => {
+    const WIND_THRESHOLD = 15;
+    if (windSpeed > WIND_THRESHOLD) {
+        //add the wind symbol
+    }
 
 }
 
