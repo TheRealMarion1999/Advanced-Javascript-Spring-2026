d5ba9581551c87df005fc810fdd97b05ca30c61a
TheRealMarion1999 <sjniesen@madisoncollege.edu>
2026-03-11 10:25:14 -0500

    almost done!

--- project 2/js/project2.js
+++ project 2/js/project2.js
@@ -67,7 +67,7 @@ const getWeather = (lat, lng) => {
             const TEMPERATURE = convert_to_burger_measurements(DATA.weatherObservation.temperature).toFixed(2);
             console.log(TEMPERATURE);
             //ohhhh ewwww am I really gonna have to put the node creation in here too...? gross...
-
+            create_weather_information(TEMPERATURE, WIND_SPEED, WIND_DIRECTON);
         }
     }
     XHR.onerror = () => {
@@ -86,11 +86,49 @@ const convert_to_burger_measurements = (celsius) => {
     return FARENHEIT;
 }
 
-const create_weather_information = (farenheit, windSpeed) => {
+const create_weather_information = (farenheit, windSpeed, windDirection) => {
+    const TEMPERATURE_TABLE = document.getElementById("temperature");
+    const TEMPERATURE_HOT_SYMBOL = String.fromCodePoint(0x2600);
+    const TEMPERATURE_COLD_SYMBOL = String.fromCodePoint(0x2744);
+    const TEMPERATURE_NEUTRAL_SYMBOL = String.fromCodePoint(0x2601);
+    const WIND_SPEED_TABLE = document.getElementById("windSpeed");
+    const WIND_SYMBOL = String.fromCodePoint(0xF04);
     const WIND_THRESHOLD = 15;
-    if (windSpeed > WIND_THRESHOLD) {
-        //add the wind symbol
+
+    const WIND_DIRECTION_TABLE = document.getElementById("windDir");
+    const WIND_DIRECTION_N = 0;
+    const WIND_DIRECTION_E = 90;
+    const WIND_DIRECTION_S = 180;
+    const WIND_DIRECTION_W = 270;
+    const WIND_DIRECTION_W_SYMBOL = String.fromCodePoint(0x2190);
+    const WIND_DIRECTION_N_SYMBOL = String.fromCodePoint(0x2191);
+    const WIND_DIRECTION_E_SYMBOL = String.fromCodePoint(0x2192);
+    const WIND_DIRECTION_S_SYMBOL = String.fromCodePoint(0x2193);
+    const WIND_DIRECTION_NW_SYMBOL = String.fromCodePoint(0x2196);
+    const WIND_DIRECTION_NE_SYMBOL = String.fromCodePoint(0x2197);
+    const WIND_DIRECTION_SE_SYMBOL = String.fromCodePoint(0x2198);
+    const WIND_DIRECTION_SW_SYMBOL = String.fromCodePoint(0x2199);
+
+    const HOT = 83;
+    const COLD = 34;
+    windSpeed = WIND_THRESHOLD
+    if (windSpeed >= WIND_THRESHOLD) {
+        WIND_SPEED_TABLE.textContent = windSpeed + WIND_SYMBOL;
+    } else {
+        WIND_SPEED_TABLE.textContent = windSpeed;
     }
+    if (farenheit >= HOT) {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_HOT_SYMBOL;
+    } else if (farenheit <= COLD) {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_COLD_SYMBOL;
+    } else {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_NEUTRAL_SYMBOL;
+    }
+    //if Wind Direction +- 11 is within the bounds of a wind direction, make its text content that symbol.
+    if (windDirection) {
+        
+    }
+
 
 }
 
