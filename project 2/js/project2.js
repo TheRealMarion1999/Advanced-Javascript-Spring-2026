ee1f6b1b359e7e01aceb3aa12255690cfdc8f2ff
Marion <sjniesen@madisoncollege.edu>
2026-03-13 19:55:46 -0500

    first draft finished

--- project 2/js/project2.js
+++ project 2/js/project2.js
@@ -16,13 +16,10 @@ const getLocation = () => {
 
     const ENTERED_POSTAL_CODE = document.getElementById("zipCode");
     const POSTALCODE = ENTERED_POSTAL_CODE.value;
-    console.log(POSTALCODE);
 
     const PARAMS = `?username=${USERNAME}&postalcode=${POSTALCODE}&country=${COUNTRY}`;
 
     const XHR = new XMLHttpRequest();
-
-    console.log(URL + PARAMS);
 
     XHR.open("get", URL + PARAMS, true);
 
@@ -51,8 +48,6 @@ const getWeather = (lat, lng) => {
     const PARAMS = `?lat=${lat}&lng=${lng}&username=${USERNAME}`;
 
     const MESSAGE = document.getElementById("loadingMessage");
-
-    console.log(URL + PARAMS);
     const XHR = new XMLHttpRequest();
 
     XHR.open("get", URL + PARAMS, true);
@@ -60,13 +55,9 @@ const getWeather = (lat, lng) => {
     XHR.onload = () => {
         if (XHR.readyState === 4) {
             const DATA = JSON.parse(XHR.responseText);
-            //this is great but... how am I gonna get this outta here?
-            console.log(DATA);
             const WIND_DIRECTON = DATA.weatherObservation.windDirection;
             const WIND_SPEED = DATA.weatherObservation.windSpeed;
             const TEMPERATURE = convert_to_burger_measurements(DATA.weatherObservation.temperature).toFixed(2);
-            console.log(TEMPERATURE);
-            //ohhhh ewwww am I really gonna have to put the node creation in here too...? gross...
             create_weather_information(TEMPERATURE, WIND_SPEED, WIND_DIRECTON);
         }
     }
@@ -87,19 +78,41 @@ const convert_to_burger_measurements = (celsius) => {
 }
 
 const create_weather_information = (farenheit, windSpeed, windDirection) => {
+    temperatureStuff(farenheit);
+    windStuff(windSpeed, windDirection);
+}
+
+const temperatureStuff = (farenheit) => {
     const TEMPERATURE_TABLE = document.getElementById("temperature");
     const TEMPERATURE_HOT_SYMBOL = String.fromCodePoint(0x2600);
     const TEMPERATURE_COLD_SYMBOL = String.fromCodePoint(0x2744);
     const TEMPERATURE_NEUTRAL_SYMBOL = String.fromCodePoint(0x2601);
+    const HOT = 83;
+    const COLD = 34;
+    if (farenheit >= HOT) {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_HOT_SYMBOL;
+    } else if (farenheit <= COLD) {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_COLD_SYMBOL;
+    } else {
+        TEMPERATURE_TABLE.textContent = TEMPERATURE_NEUTRAL_SYMBOL;
+    }
+}
+
+const windStuff = (windSpeed, windDirection) => {
     const WIND_SPEED_TABLE = document.getElementById("windSpeed");
     const WIND_SYMBOL = String.fromCodePoint(0xF04);
     const WIND_THRESHOLD = 15;
 
     const WIND_DIRECTION_TABLE = document.getElementById("windDir");
+    //these are giving me grief because I'm too used to Eulers and being able to go backwards on an axis by going negative. I can't do that here.
     const WIND_DIRECTION_N = 0;
+    const WIND_DIRECTION_NE = range(23, 77, 1);
     const WIND_DIRECTION_E = 90;
+    const WIND_DIRECTION_SE = range(112, 158, 1);
     const WIND_DIRECTION_S = 180;
+    const WIND_DIRECTION_SW = range(202, 248, 1);
     const WIND_DIRECTION_W = 270;
+    const WIND_DIRECTION_NW = range(292, 338, 1);
     const WIND_DIRECTION_W_SYMBOL = String.fromCodePoint(0x2190);
     const WIND_DIRECTION_N_SYMBOL = String.fromCodePoint(0x2191);
     const WIND_DIRECTION_E_SYMBOL = String.fromCodePoint(0x2192);
@@ -109,27 +122,35 @@ const create_weather_information = (farenheit, windSpeed, windDirection) => {
     const WIND_DIRECTION_SE_SYMBOL = String.fromCodePoint(0x2198);
     const WIND_DIRECTION_SW_SYMBOL = String.fromCodePoint(0x2199);
 
-    const HOT = 83;
-    const COLD = 34;
-    windSpeed = WIND_THRESHOLD
     if (windSpeed >= WIND_THRESHOLD) {
         WIND_SPEED_TABLE.textContent = windSpeed + WIND_SYMBOL;
     } else {
         WIND_SPEED_TABLE.textContent = windSpeed;
     }
-    if (farenheit >= HOT) {
-        TEMPERATURE_TABLE.textContent = TEMPERATURE_HOT_SYMBOL;
-    } else if (farenheit <= COLD) {
-        TEMPERATURE_TABLE.textContent = TEMPERATURE_COLD_SYMBOL;
-    } else {
-        TEMPERATURE_TABLE.textContent = TEMPERATURE_NEUTRAL_SYMBOL;
-    }
     //if Wind Direction +- 11 is within the bounds of a wind direction, make its text content that symbol.
-    if (windDirection) {
-        
+    //TODO: Make this a switch. I don't know how to do declarations as cases but there's a way to do it in Godot so there's prolly a way to do it here.
+    //Yanderedev "There has to be a better way to do this".png
+    if (WIND_DIRECTION_NW.includes(windDirection)) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_NW_SYMBOL;
+    } else if (windDirection >= WIND_DIRECTION_W) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_W_SYMBOL;
+    } else if (WIND_DIRECTION_SW.includes(windDirection)) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_SW_SYMBOL;
+    } else if (windDirection >= WIND_DIRECTION_S) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_S_SYMBOL;
+    } else if (WIND_DIRECTION_SE.includes(windDirection)) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_W_SYMBOL;
+    } else if (windDirection >= WIND_DIRECTION_E) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_E_SYMBOL;
+    } else if (WIND_DIRECTION_NE.includes(windDirection)) {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_NE_SYMBOL;
+    } else {
+        WIND_DIRECTION_TABLE.textContent = WIND_DIRECTION_N_SYMBOL;
     }
-
-
 }
+
+//Shamlessly stolen from Stackoverflow, I didn't feel like typing out a 41 integer array for problem-solving with wind directions.
+//here's the thread I took it from: https://stackoverflow.com/questions/3895478/does-javascript-have-a-method-like-range-to-generate-a-range-within-the-supp
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 
 window.onload = init;
