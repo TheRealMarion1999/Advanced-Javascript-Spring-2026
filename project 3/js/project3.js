d1fd967139e3664496b126da5a019662e6ee504c
Marion <sjniesen@madisoncollege.edu>
2026-04-13 17:41:24 -0500

    More Project 3

--- project 3/js/project3.js
+++ project 3/js/project3.js
@@ -1,40 +1,51 @@
 const URL = "https://ghu8xhzgfe.execute-api.us-east-1.amazonaws.com/tasks/";
 const MY_ID = "2934096";
 
-const HTTP = require("http");
-let server = HTTP.createServer((req, res) => {
-    res.write("Hello!!! Hi!!!");
-})
 
 const init = () => {
     const TEXTINPUT = document.getElementById("toDoInput");
     const BUTTON = document.getElementById("toDoButton");
     const LOAD_MESSAGE = document.getElementById("loadMessage");
     const OUTPUT = document.getElementById("output");
-
+    get_todo_list()
     BUTTON.addEventListener("click", () => 
     {
-        create_table_point();
+        //call a POST statement, after it's done call a GET again..
+        get_todo_list();
     });
 }
 
-/* 
-when creating the table elements, we want to have a unified structure like this:
-<td><a>(trashcan ascii image)</a>(task name)</td>
-*/
+const get_todo_list = () => {
+    //for each of the entries in the json, create a table point
+    create_table_point();
+}
+
+const create_new_json_point = async () => {
+    const XHR = new XMLHttpRequest
+    const PARAMS = {
+    'StudentId' : '2934096',
+    'Description' : 'Record Test'
+    }
+    const NEW_TASK_RESPONSE = await fetch()
+}
 
 const create_table_point = () => {
     const TABLE = document.getElementById("output");
     const TABLE_ROW = document.createElement("tr")
+    const TEXT = document.createTextNode("TEXT")
     const TABLE_POINT = document.createElement("td");
     const TRASH_CAN_ICON = String.fromCodePoint(0x1F5D1);
-    const DELETE_ICON = document.createElement("a");
+    const DELETE_ICON = document.createElement("button");
     DELETE_ICON.textContent = TRASH_CAN_ICON;
-    DELETE_ICON.href = "";
     TABLE_POINT.appendChild(DELETE_ICON);
-    TABLE_POINT.append("TEXT");
+    TABLE_POINT.appendChild(TEXT);
     TABLE_ROW.appendChild(TABLE_POINT)
     TABLE.appendChild(TABLE_ROW);
+
+    DELETE_ICON.addEventListener("click", () => {
+        //This will call a DELETE statement to the API, then call a GET again
+            get_todo_list();
+    })
 }
 
 /**
